47b1ef9dd7ab858fd96b149bfcd46f31bdb3e412
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-02-26 23:20:06 +0000

    Included app.py, which is used to run the app.

---
+++ my_app/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+from my_app import create_app
+from my_app.config import DevelopmentConfig
+
+app = create_app(DevelopmentConfig)
+
+if __name__ == '__main__':
+    app.run(debug=True)
