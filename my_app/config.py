1f86e2c157dc7e02391576e7f1a6b4902738a021
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-04 23:48:35 +0000

    Configuration variables are no longer defined inside the __init__ method (otherwise, an error would occur).

--- my_app/config.py
+++ my_app/config.py
@@ -2,28 +2,22 @@ import pathlib
 
 
 class Config(object):
-    def __init__(self):
-        self.SECRET_KEY = b'gjyMT4Ju-ek9u4MDeOhpHQ'
-        self.SQLALCHEMY_TRACK_MODIFICATIONS = False
-        self.DATA_PATH = pathlib.Path(__file__).parent.parent.joinpath("data")
-        self.SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(self.DATA_PATH.joinpath('example.sqlite'))
+    SECRET_KEY = b'gjyMT4Ju-ek9u4MDeOhpHQ'
+    WTF_CSRF_SECRET_KEY = "crFAuXFCPKbKWw8JAKfnSA"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    DATA_PATH = pathlib.Path(__file__).parent.parent.joinpath("data")
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(DATA_PATH.joinpath('example.sqlite'))
 
 
 class ProductionConfig(Config):
-    def __init__(self):
-        super().__init__()
-        self.TESTING = False
+    TESTING = False
 
 
 class DevelopmentConfig(Config):
-    def __init__(self):
-        super().__init__()
-        self.TESTING = False
-        self.SQLALCHEMY_ECHO = True
+    TESTING = False
+    SQLALCHEMY_ECHO = True
 
 
 class TestingConfig(Config):
-    def __init__(self):
-        super().__init__()
-        self.TESTING = True
-        self.SQLALCHEMY_ECHO = True
+    TESTING = True
+    SQLALCHEMY_ECHO = True
