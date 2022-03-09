607c3f03fe0a824e4b499e9b9f782564a0cf27ae
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-09 16:49:31 +0000

    Included an email validator and introduced a new LoginForm class (with email and password validators).

--- my_app/auth/forms.py
+++ my_app/auth/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import StringField, PasswordField, EmailField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+from my_app.models import User
 
 
 class SignupForm(FlaskForm):
@@ -11,3 +13,24 @@ class SignupForm(FlaskForm):
     password_repeat = PasswordField(label='Repeat Password',
                                     validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
 
+    def validate_email(self, email):
+        users = User.query.filter_by(email=email.data).first()
+        if users is not None:
+            raise ValidationError('An account is already registered for that email address')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField(label='Email address', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    remember_me = BooleanField(label='Remember me')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError('That email address is not registered')
+        return user
+
+    def validate_password(self, email, password):
+        user = self.validate_email(email)
+        if user.password != password.data:
+            raise ValidationError('The password is incorrect')
