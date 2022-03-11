e26c8f35fd5a48a604c034ed559797609837d75b
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-11 23:29:43 +0000

    Modified the signup route so that new users are stored on the database.

--- my_app/auth/routes.py
+++ my_app/auth/routes.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
-from my_app.auth.forms import SignupForm
+from flask import Blueprint, render_template, flash, redirect, url_for, request
+from sqlalchemy.exc import IntegrityError
 
-auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+from my_app import db
+from my_app.auth.forms import SignupForm, LoginForm
+from my_app.models import User
+
+auth_bp = Blueprint('auth', __name__)
 
 
 '''@auth_bp.route('/')
@@ -9,11 +13,19 @@ def index():
     return render_template('authorization_page.html', )'''
 
 
-@auth_bp.route('/', methods=['GET', 'POST'])
+@auth_bp.route('/auth/', methods=['GET', 'POST'])
 def signup():
-    form = SignupForm()
+    form = SignupForm(request.form)
     if form.validate_on_submit():
-        name = form.first_name.data
-        return f"Hello, {name}. You are signed up."
-    print(form)
+        user = User(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data)
+        user.set_password(form.password.data)
+        try:
+            db.session.add(user)
+            db.session.commit()
+            flash(f"Hello, {user.first_name} {user.last_name}. You are signed up.")
+        except IntegrityError:
+            db.session.rollback()
+            flash(f'Unable to register {form.email.data}.', 'error')
+            return redirect(url_for('auth.signup'))
+        return redirect(url_for('main.index'))
     return render_template('authorization_page.html', form=form)
