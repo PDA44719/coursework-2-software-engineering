b51dc64f25f7ced6faf564336583c4b1118ee843
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-16 22:40:27 +0000

    Setting the route to "login_required" and displaying the user's firstname after logging in.

--- my_app/main/routes.py
+++ my_app/main/routes.py
@@ -1,8 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
+from flask_login import current_user, login_required
 
 main_bp = Blueprint('main', __name__)
 
 
 @main_bp.route('/')
+@login_required
 def index():
+    if not current_user.is_anonymous:
+        name = current_user.first_name
+        flash(f'Hello {name}. ')
     return render_template('main_page.html')
