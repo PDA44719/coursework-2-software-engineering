ea862963f62dc877101098430d28ca83ccb131da
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-16 22:36:53 +0000

    Incorporated a database (SQLAlchemy) and a LoginManager into the app.

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 csrf = CSRFProtect()
+db = SQLAlchemy()
+login_manager = LoginManager()
 
 
 def create_app(config_class_name):
@@ -12,14 +15,21 @@ def create_app(config_class_name):
     app = Flask(__name__)
     app.config.from_object(config_class_name)
     csrf.init_app(app)
+    db.init_app(app)
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    with app.app_context():
+        # Import Dash application
+        from dash_app.dash import init_dashboard
+        app = init_dashboard(app)
+
+        from my_app.models import User
+        db.create_all()
 
     from my_app.auth.routes import auth_bp
     from my_app.main.routes import main_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(main_bp)
 
-    with app.app_context():
-        # Import Dash application
-        from dash_app.dash import init_dashboard
-        app = init_dashboard(app)
     return app
