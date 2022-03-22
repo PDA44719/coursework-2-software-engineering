c2f72a9d0e1a0ea399b984e5335c9dc656a95e6a
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-22 23:27:16 +0000

    Specified the destination where the profile photos will be stored.

--- my_app/config.py
+++ my_app/config.py
@@ -5,8 +5,9 @@ class Config(object):
     SECRET_KEY = b'gjyMT4Ju-ek9u4MDeOhpHQ'
     WTF_CSRF_SECRET_KEY = "crFAuXFCPKbKWw8JAKfnSA"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    DATA_PATH = pathlib.Path(__file__).parent.parent.joinpath("data")
+    DATA_PATH = pathlib.Path(__file__).parent.parent.joinpath("my_app")
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(DATA_PATH.joinpath('example.sqlite'))
+    UPLOADED_PHOTOS_DEST = pathlib.Path(__file__).parent.joinpath("static/img")
 
 
 class ProductionConfig(Config):
