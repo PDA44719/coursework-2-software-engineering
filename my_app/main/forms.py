e2e94e76d4e3d9762fd96717109eea47ba2efde6
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-03-31 22:49:11 +0100

    Allowed users to keep the username they previously had.

--- my_app/main/forms.py
+++ my_app/main/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, PasswordField, EmailField, BooleanField
+from wtforms import StringField, PasswordField, EmailField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, EqualTo, ValidationError
 from my_app import photos
 from my_app.models import Profile
+from flask_login import current_user
 
 
 class ProfileForm(FlaskForm):
@@ -12,5 +13,6 @@ class ProfileForm(FlaskForm):
 
     def validate_username(self, username):
         profile = Profile.query.filter_by(username=username.data).first()
-        if profile is not None:
+        # If that username is already being used by someone who is not the current user
+        if profile is not None and profile.user != current_user:
             raise ValidationError('An account is already registered with that username')
